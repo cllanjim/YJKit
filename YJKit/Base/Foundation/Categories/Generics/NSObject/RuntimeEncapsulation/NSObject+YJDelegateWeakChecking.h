b4f51ea8e2374cbd24192c70ee6f138eef41f460
huang-kun <jack-huang-developer@foxmail.com>
2016-07-18 19:48:46 +0800

    Fix UITextView crashes. Change iOS_version macro.

---
+++ YJKit/Base/Foundation/Categories/Generics/NSObject/RuntimeEncapsulation/NSObject+YJDelegateWeakChecking.h
@@ -0,0 +1,13 @@
+//
+//  NSObject+YJDelegateWeakChecking.h
+//  YJKit
+//
+//  Created by Jack Huang on 16/7/18.
+//  Copyright © 2016年 huang-kun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (YJDelegateWeakChecking)
+
+@end
---
+++ YJKit/Base/Foundation/Categories/Generics/NSObject/RuntimeEncapsulation/NSObject+YJDelegateWeakChecking.m
@@ -0,0 +1,13 @@
+//
+//  NSObject+YJDelegateWeakChecking.m
+//  YJKit
+//
+//  Created by Jack Huang on 16/7/18.
+//  Copyright © 2016年 huang-kun. All rights reserved.
+//
+
+#import "NSObject+YJDelegateWeakChecking.h"
+
+@implementation NSObject (YJDelegateWeakChecking)
+
+@end
