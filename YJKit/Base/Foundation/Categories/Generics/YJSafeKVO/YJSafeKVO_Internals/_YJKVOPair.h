b4312c5c306fa353fefe9786a5866e5b7794aea3
huang-kun <jack-huang-developer@foxmail.com>
2016-07-27 16:02:47 +0800

    Improve YJSafeKVO grouping feature.

---
+++ YJKit/Base/Foundation/Categories/Generics/YJSafeKVO/YJSafeKVO_Internals/_YJKVOPair.h
@@ -0,0 +1,13 @@
+//
+//  _YJKVOPair.h
+//  YJKit
+//
+//  Created by huang-kun on 16/7/27.
+//  Copyright © 2016年 huang-kun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface _YJKVOPair : NSObject
+
+@end
---
+++ YJKit/Base/Foundation/Categories/Generics/YJSafeKVO/YJSafeKVO_Internals/_YJKVOPair.m
@@ -0,0 +1,13 @@
+//
+//  _YJKVOPair.m
+//  YJKit
+//
+//  Created by huang-kun on 16/7/27.
+//  Copyright © 2016年 huang-kun. All rights reserved.
+//
+
+#import "_YJKVOPair.h"
+
+@implementation _YJKVOPair
+
+@end
